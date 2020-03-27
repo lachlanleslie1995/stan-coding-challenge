1fd9ee30381672f9562693747e75c933c759cfd8
lachlanleslie1995 <lachlan.leslie1995@gmail.com>
2020-03-28 09:33:26 +1100

    Restructure file system so that it makes more sense

--- .babelrc.js
+++ .babelrc.js
@@ -2,11 +2,7 @@ module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
   env: {
     production: {
-      plugins: [
-        "babel-plugin-jsx-remove-data-test-id",
-        "transform-runtime",
-        "transform-regenerator"
-      ]
+      plugins: ["babel-plugin-jsx-remove-data-test-id"]
     }
   }
 };
